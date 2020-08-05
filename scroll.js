88319958a92c87d7aeac55eaf3581c2d8e9ff8ec
Hichem Fantar <mobyfreak123@gmail.com>
2020-08-05 03:07:37 +0100

    add scroll header

---
+++ scroll.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementsByTagName("header")[0].style.padding = "10px 0px";
+  } else {
+    document.getElementsByTagName("header")[0].style.padding = "20px 0px";
+  }
+}
